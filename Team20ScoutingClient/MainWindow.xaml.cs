f8b4f6c27de41e757d75c779e476d36f2d4c3aff
Andrew S <yipten@outlook.com>
2019-05-15 16:26:19 -0400

    5/14 update
    -rename some stuff
    -add more line graphs to teams tab for testing
    -add preliminary Bluetooth management code

--- Team20ScoutingClient/MainWindow.xaml.cs
+++ Team20ScoutingClient/MainWindow.xaml.cs
@@ -7,6 +7,8 @@ namespace Team20ScoutingClient {
         private DBClient client;
 
         private LineGraph[] teamLineGraphs;
+        private LineGraph teamSandstormCargo;
+        private LineGraph teamSandstormPanel;
         private LineGraph teamTeleopCargo;
         private LineGraph teamTeleopPanel;
 
@@ -24,21 +26,23 @@ namespace Team20ScoutingClient {
         }
 
         private void InitTabs() {
-            TeamsTabInit();
+            InitTeamsTab();
         }
 
         private void RefreshTabs() {
-            TeamsTabRefresh();
+            RefreshTeamsTab();
         }
 
         private void TeamCB_SelectionChanged(object sender, SelectionChangedEventArgs e) {
-            TeamsTabRefresh();
+            RefreshTeamsTab();
         }
 
-        private void TeamsTabInit() {
-            teamTeleopCargo = new LineGraph(teamTeleopCargoCanvas, 0, 10, "Cargo in Sandstorm", "Match", "Cargo");
-            teamTeleopPanel = new LineGraph(teamTeleopPanelCanvas, 0, 10, "Hatch Panels in Sandstorm", "Match", "Hatch Panels");
-            teamLineGraphs = new LineGraph[] { teamTeleopCargo, teamTeleopPanel };
+        private void InitTeamsTab() {
+            teamSandstormCargo = new LineGraph(teamSandstormCargoCanvas, 0, 10, "Cargo in Sandstorm");
+            teamSandstormPanel = new LineGraph(teamSandstormPanelCanvas, 0, 10, "Hatch Panels in Sandstorm");
+            teamTeleopCargo = new LineGraph(teamTeleopCargoCanvas, 0, 10, "Cargo in Teleop");
+            teamTeleopPanel = new LineGraph(teamTeleopPanelCanvas, 0, 10, "Hatch Panels in Teleop");
+            teamLineGraphs = new LineGraph[] { teamSandstormCargo, teamSandstormPanel, teamTeleopCargo, teamTeleopPanel };
             if (client.GetData("teams", new string[] { "number", "name" }, orderBy: "number")) {
                 TeamCB.Items.Clear();
                 for (int i = 0; i < client.Data[0].Count - 1; i++)
@@ -47,20 +51,57 @@ namespace Team20ScoutingClient {
             }
         }
 
-        private void TeamsTabRefresh() {
+        private void RefreshTeamsTab() {
             if (client.GetData("teams", new string[] { "number", "name" }, filter: "number", filterValue: TeamCB.SelectedItem.ToString())) {
                 TeamsTabTitle.Text = client.Data[0][0] + " - " + client.Data[1][0];
             }
             foreach (LineGraph lineGraph in teamLineGraphs)
                 lineGraph.LinePlots.Clear();
-            if (client.GetData("teleop", new string[] { "cargoDeliverShip", "cargoDeliverRocket", "panelDeliverShip", "panelDeliverRocket" }, filter: "team", filterValue: TeamCB.SelectedItem.ToString(), "match")) {
-                teamTeleopCargo.LinePlots.Add(new LinePlot(client.Data[0], Brushes.Green, true));
-                teamTeleopCargo.LinePlots.Add(new LinePlot(client.Data[1], Brushes.Green, false));
+            if (client.GetData("sandstorm", new string[] { "cargoCollect", "cargoDeliverShip", "cargoDeliverRocket", "cargoDrop" }, filter: "team", filterValue: TeamCB.SelectedItem.ToString(), orderBy: "match")) {
+                teamSandstormCargo.LinePlots.Add(new LinePlot(client.Data[0], Brushes.Yellow, false));
+                teamSandstormCargo.LinePlots.Add(new LinePlot(client.Data[1], Brushes.Green, true));
+                teamSandstormCargo.LinePlots.Add(new LinePlot(client.Data[2], Brushes.Green, false));
+                teamSandstormCargo.LinePlots.Add(new LinePlot(client.Data[3], Brushes.Red, false));
+            }
+            if (client.GetData("sandstorm", new string[] { "panelCollectStation", "panelCollectFloor", "panelDeliverShip", "panelDeliverRocket", "panelDrop" }, filter: "team", filterValue: TeamCB.SelectedItem.ToString(), orderBy: "match")) {
+                teamSandstormPanel.LinePlots.Add(new LinePlot(client.Data[0], Brushes.Yellow, false));
+                teamSandstormPanel.LinePlots.Add(new LinePlot(client.Data[1], Brushes.Yellow, true));
+                teamSandstormPanel.LinePlots.Add(new LinePlot(client.Data[2], Brushes.Green, true));
+                teamSandstormPanel.LinePlots.Add(new LinePlot(client.Data[3], Brushes.Green, false));
+                teamSandstormPanel.LinePlots.Add(new LinePlot(client.Data[4], Brushes.Red, false));
+            }
+            if (client.GetData("teleop", new string[] { "cargoCollectStation", "cargoCollectFloor", "cargoDeliverShip", "cargoDeliverRocket", "cargoDrop" }, filter: "team", filterValue: TeamCB.SelectedItem.ToString(), orderBy: "match")) {
+                teamTeleopCargo.LinePlots.Add(new LinePlot(client.Data[0], Brushes.Yellow, false));
+                teamTeleopCargo.LinePlots.Add(new LinePlot(client.Data[1], Brushes.Yellow, true));
+                teamTeleopCargo.LinePlots.Add(new LinePlot(client.Data[2], Brushes.Green, true));
+                teamTeleopCargo.LinePlots.Add(new LinePlot(client.Data[3], Brushes.Green, false));
+                teamTeleopCargo.LinePlots.Add(new LinePlot(client.Data[4], Brushes.Red, false));
+            }
+            if (client.GetData("teleop", new string[] { "panelCollectStation", "panelCollectFloor", "panelDeliverShip", "panelDeliverRocket", "panelDrop" }, filter: "team", filterValue: TeamCB.SelectedItem.ToString(), orderBy: "match")) {
+                teamTeleopPanel.LinePlots.Add(new LinePlot(client.Data[0], Brushes.Yellow, false));
+                teamTeleopPanel.LinePlots.Add(new LinePlot(client.Data[1], Brushes.Yellow, true));
                 teamTeleopPanel.LinePlots.Add(new LinePlot(client.Data[2], Brushes.Green, true));
                 teamTeleopPanel.LinePlots.Add(new LinePlot(client.Data[3], Brushes.Green, false));
+                teamTeleopPanel.LinePlots.Add(new LinePlot(client.Data[4], Brushes.Red, false));
             }
             foreach (LineGraph lineGraph in teamLineGraphs)
                 lineGraph.Draw();
         }
-    }
+
+		private void InitDataTab() {
+
+		}
+
+		private void RefreshDataTab() {
+
+		}
+
+		private void EnableReceivingButton_Click(object sender, RoutedEventArgs e) {
+
+		}
+
+		private void DisableReceivingButton_Click(object sender, RoutedEventArgs e) {
+
+		}
+	}
 }
