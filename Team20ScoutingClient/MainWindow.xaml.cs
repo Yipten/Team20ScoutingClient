9e600bfba2a48ba66d6becce6a19098350ebed95
Andrew S <yipten@outlook.com>
2019-07-06 15:53:15 -0400

    -change variable names -organize code into regions -update README

--- Team20ScoutingClient/MainWindow.xaml.cs
+++ Team20ScoutingClient/MainWindow.xaml.cs
@@ -1,20 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 
 namespace Team20ScoutingClient {
 	public partial class MainWindow : Window {
+		#region Main
 		private DBClient db;
-		private BTClient bt;
-
-		private LineGraph[] teamLineGraphs;
-		private LineGraph teamSandstormCargo;
-		private LineGraph teamSandstormPanel;
-		private LineGraph teamTeleopCargo;
-		private LineGraph teamTeleopPanel;
-
-		private Stat testStat;
 
 		private bool init;
 
@@ -22,7 +15,6 @@ namespace Team20ScoutingClient {
 			InitializeComponent();
 
 			db = new DBClient("C:/Users/Andrew/source/repos/Team20ScoutingClient/2019ScoutingData.sqlite");
-			bt = new BTClient("C:/Users/Andrew/Desktop/", ref BTStatus);
 
 			init = false;
 
@@ -30,78 +22,103 @@ namespace Team20ScoutingClient {
 			RefreshTabs();
 		}
 
-		private void RefreshButton_Click(object sender, RoutedEventArgs e) {
-			RefreshTabs();
-		}
-
 		private void InitTabs() {
-			InitTeamsTab();
+			InitTeamStatsTab();
+			InitTeamTrendsTab();
 			InitDataTab();
 			init = true;
 		}
 
 		private void RefreshTabs() {
-			RefreshTeamsTab();
+			RefreshTeamStatsTab();
+			RefreshTeamTrendsTab();
 			RefreshDataTab();
 		}
 
-		private void TeamCB_SelectionChanged(object sender, SelectionChangedEventArgs e) {
-			if (init)
-				RefreshTeamsTab();
+		private void RefreshButton_Click(object sender, RoutedEventArgs e) {
+			RefreshTabs();
+		}
+		#endregion
+
+		#region Team Stats
+		private Stat[] stats;
+		private Stat habLineL1Stat;
+		private Stat testStat;
+
+		private void InitTeamStatsTab() {
+			//habLineL1Stat = new Stat();
+			testStat = new Stat(() => {
+				db.GetData("teleop", new string[] { "cargoDeliverShip" }, filter: "team", filterValue: TeamTrendsSelection.SelectedItem.ToString());
+				return db.Data[0];
+			}, x => {
+				double total = 0;
+				foreach (double item in x)
+					total += item;
+				return Math.Round(total / x.Count, 2);
+			}, "avg tele cargo to ship", "cargo balls");
+			stats = new Stat[] { habLineL1Stat, testStat };
 		}
 
-		private void InitTeamsTab() {
+		private void RefreshTeamStatsTab() {
+			foreach (Stat s in stats)
+				s.Calculate();
+			testStatTB.Text = testStat.ToString();
+		}
+
+		private void TeamStatsSelection_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+			if (init)
+				RefreshTeamStatsTab();
+		}
+		#endregion
+
+		#region Team Trends
+		//TODO: make LineGraph similar to Stat by having function passed in constructor
+		private LineGraph[] teamLineGraphs;
+		private LineGraph teamSandstormCargo;
+		private LineGraph teamSandstormPanel;
+		private LineGraph teamTeleopCargo;
+		private LineGraph teamTeleopPanel;
+
+		private void InitTeamTrendsTab() {
 			teamSandstormCargo = new LineGraph(ref teamSandstormCargoCanvas, 0, 10, "Cargo in Sandstorm");
 			teamSandstormPanel = new LineGraph(ref teamSandstormPanelCanvas, 0, 10, "Hatch Panels in Sandstorm");
 			teamTeleopCargo = new LineGraph(ref teamTeleopCargoCanvas, 0, 10, "Cargo in Teleop");
 			teamTeleopPanel = new LineGraph(ref teamTeleopPanelCanvas, 0, 10, "Hatch Panels in Teleop");
 			teamLineGraphs = new LineGraph[] { teamSandstormCargo, teamSandstormPanel, teamTeleopCargo, teamTeleopPanel };
 			if (db.GetData("teams", new string[] { "number", "name" }, orderBy: "number")) {
-				TeamCB.Items.Clear();
+				TeamTrendsSelection.Items.Clear();
 				for (int i = 0; i < db.Data[0].Count - 1; i++)
-					TeamCB.Items.Add(db.Data[0][i]); // + " - " + client.Data[1][i]);
-				TeamCB.SelectedItem = TeamCB.Items[0];
+					TeamTrendsSelection.Items.Add(db.Data[0][i]);
+				TeamTrendsSelection.SelectedItem = TeamTrendsSelection.Items[0];
 			}
-
-			testStat = new Stat(() => {
-				db.GetData("teleop", new string[] { "cargoDeliverShip" }, filter: "team", filterValue: TeamCB.SelectedItem.ToString());
-				return db.Data[0];
-			},
-			x => {
-				double total = 0;
-				foreach (double item in x)
-					total += item;
-				return Math.Round(total / x.Count, 2);
-			},
-			"avg tele cargo to ship", "cargo balls");
 		}
 
-		private void RefreshTeamsTab() {
-			if (db.GetData("teams", new string[] { "number", "name" }, filter: "number", filterValue: TeamCB.SelectedItem.ToString()))
+		private void RefreshTeamTrendsTab() {
+			if (db.GetData("teams", new string[] { "number", "name" }, filter: "number", filterValue: TeamTrendsSelection.SelectedItem.ToString(), orderBy: "number"))
 				TeamsTabTitle.Text = db.Data[0][0] + " - " + db.Data[1][0];
 			foreach (LineGraph lineGraph in teamLineGraphs)
 				lineGraph.LinePlots.Clear();
-			if (db.GetData("sandstorm", new string[] { "cargoCollect", "cargoDeliverShip", "cargoDeliverRocket", "cargoDrop" }, filter: "team", filterValue: TeamCB.SelectedItem.ToString(), orderBy: "match")) {
+			if (db.GetData("sandstorm", new string[] { "cargoCollect", "cargoDeliverShip", "cargoDeliverRocket", "cargoDrop" }, filter: "team", filterValue: TeamTrendsSelection.SelectedItem.ToString(), orderBy: "match")) {
 				teamSandstormCargo.LinePlots.Add(new LinePlot(db.Data[0], Brushes.Yellow, false));
 				teamSandstormCargo.LinePlots.Add(new LinePlot(db.Data[1], Brushes.Green, true));
 				teamSandstormCargo.LinePlots.Add(new LinePlot(db.Data[2], Brushes.Green, false));
 				teamSandstormCargo.LinePlots.Add(new LinePlot(db.Data[3], Brushes.Red, false));
 			}
-			if (db.GetData("sandstorm", new string[] { "panelCollectStation", "panelCollectFloor", "panelDeliverShip", "panelDeliverRocket", "panelDrop" }, filter: "team", filterValue: TeamCB.SelectedItem.ToString(), orderBy: "match")) {
+			if (db.GetData("sandstorm", new string[] { "panelCollectStation", "panelCollectFloor", "panelDeliverShip", "panelDeliverRocket", "panelDrop" }, filter: "team", filterValue: TeamTrendsSelection.SelectedItem.ToString(), orderBy: "match")) {
 				teamSandstormPanel.LinePlots.Add(new LinePlot(db.Data[0], Brushes.Yellow, false));
 				teamSandstormPanel.LinePlots.Add(new LinePlot(db.Data[1], Brushes.Yellow, true));
 				teamSandstormPanel.LinePlots.Add(new LinePlot(db.Data[2], Brushes.Green, true));
 				teamSandstormPanel.LinePlots.Add(new LinePlot(db.Data[3], Brushes.Green, false));
 				teamSandstormPanel.LinePlots.Add(new LinePlot(db.Data[4], Brushes.Red, false));
 			}
-			if (db.GetData("teleop", new string[] { "cargoCollectStation", "cargoCollectFloor", "cargoDeliverShip", "cargoDeliverRocket", "cargoDrop" }, filter: "team", filterValue: TeamCB.SelectedItem.ToString(), orderBy: "match")) {
+			if (db.GetData("teleop", new string[] { "cargoCollectStation", "cargoCollectFloor", "cargoDeliverShip", "cargoDeliverRocket", "cargoDrop" }, filter: "team", filterValue: TeamTrendsSelection.SelectedItem.ToString(), orderBy: "match")) {
 				teamTeleopCargo.LinePlots.Add(new LinePlot(db.Data[0], Brushes.Yellow, false));
 				teamTeleopCargo.LinePlots.Add(new LinePlot(db.Data[1], Brushes.Yellow, true));
 				teamTeleopCargo.LinePlots.Add(new LinePlot(db.Data[2], Brushes.Green, true));
 				teamTeleopCargo.LinePlots.Add(new LinePlot(db.Data[3], Brushes.Green, false));
 				teamTeleopCargo.LinePlots.Add(new LinePlot(db.Data[4], Brushes.Red, false));
 			}
-			if (db.GetData("teleop", new string[] { "panelCollectStation", "panelCollectFloor", "panelDeliverShip", "panelDeliverRocket", "panelDrop" }, filter: "team", filterValue: TeamCB.SelectedItem.ToString(), orderBy: "match")) {
+			if (db.GetData("teleop", new string[] { "panelCollectStation", "panelCollectFloor", "panelDeliverShip", "panelDeliverRocket", "panelDrop" }, filter: "team", filterValue: TeamTrendsSelection.SelectedItem.ToString(), orderBy: "match")) {
 				teamTeleopPanel.LinePlots.Add(new LinePlot(db.Data[0], Brushes.Yellow, false));
 				teamTeleopPanel.LinePlots.Add(new LinePlot(db.Data[1], Brushes.Yellow, true));
 				teamTeleopPanel.LinePlots.Add(new LinePlot(db.Data[2], Brushes.Green, true));
@@ -110,13 +127,31 @@ namespace Team20ScoutingClient {
 			}
 			foreach (LineGraph lineGraph in teamLineGraphs)
 				lineGraph.Draw();
-
-			testStat.Calculate();
-			testStatTB.Text = testStat.ToString();
 		}
 
-		private void InitDataTab() {
+		private void TeamTrendsSelection_SelectionChanged(object sender, SelectionChangedEventArgs e) {
+			if (init)
+				RefreshTeamTrendsTab();
+		}
+		#endregion
 
+		#region Match History
+
+		#endregion
+
+		#region Match Predictions
+
+		#endregion
+
+		#region Scoring
+
+		#endregion
+
+		#region Data Management
+		private BTClient bt;
+
+		private void InitDataTab() {
+			bt = new BTClient("C:/Users/Andrew/Desktop/", ref BTStatus);
 		}
 
 		private void RefreshDataTab() {
@@ -126,5 +161,6 @@ namespace Team20ScoutingClient {
 		private void ReceiveButton_Click(object sender, RoutedEventArgs e) {
 			bt.ReceiveFile();
 		}
+		#endregion
 	}
 }
