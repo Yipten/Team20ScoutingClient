a1a12de20f005e0fcb0e46b0e87a815daf2dc71b
Andrew S <yipten@outlook.com>
2019-09-01 16:51:11 -0400

    update BT status output

--- Team20ScoutingClient/BTClient.cs
+++ Team20ScoutingClient/BTClient.cs
@@ -10,37 +10,56 @@ namespace Team20ScoutingClient {
 
 		private string filePath;
 		private int numReceiving;
-		private string[] statusItems;
 
 		public BTClient(string filePath, ref TextBlock statusDisplay) {
 			this.filePath = filePath;
 			this.statusDisplay = statusDisplay;
 			numReceiving = 0;
-			statusItems = new string[2];
-			statusItems[0] = "disabled";
+			UpdateStatus();
 		}
 
-		public async void ReceiveFile() {
+		public async void ReceiveFile() {       //TODO: research cancellation to be able to cancel pending file transfers
 			if (BluetoothRadio.IsSupported) {
 				if (numReceiving < 6) {
-					statusDisplay.Text = (++numReceiving).ToString();
+					numReceiving++;
+					UpdateStatus();
+					//allow radio to be connected to by other devices
 					BluetoothRadio.PrimaryRadio.Mode = RadioMode.Connectable;
+					//wait for file to be received in a separate thread
 					await Task.Run(() => {
 						ObexListener listener = new ObexListener(ObexTransport.Bluetooth);
+						//allow files to be received
 						listener.Start();
+						//wait for file to be received
 						ObexListenerContext context = listener.GetContext();
+						//get file information
 						ObexListenerRequest request = context.Request;
+						//keep original file name
 						string[] pathSplits = request.RawUrl.Split('/');
 						string fileName = pathSplits[pathSplits.Length - 1];
+						//save file
 						request.WriteFile(filePath + fileName);
+						//release resources
 						listener.Stop();
 						listener.Close();
 					});
-					statusDisplay.Text = (--numReceiving).ToString();
+					numReceiving--;
 				} else
 					MessageBox.Show("Number of pending transfers is limited to 6", "FYI", MessageBoxButton.OK, MessageBoxImage.Information);
 			} else
-				MessageBox.Show("Bluetooth must be enabled on your device for this function to work", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+				MessageBox.Show("Bluetooth must be enabled on your device in order to transfer files", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+			UpdateStatus();
+		}
+
+		public void UpdateStatus() {
+			string str;
+			if (numReceiving > 0)
+				str = numReceiving + " pending transfers";
+			else if (BluetoothRadio.IsSupported)
+				str = "ready";
+			else
+				str = "bluetooth disabled";
+			statusDisplay.Text = str;
 		}
 	}
 }
