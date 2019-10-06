fd9c149886321bcf4244ec4dbd296807c782892a
Andrew S <yipten@outlook.com>
2019-10-06 16:59:22 -0400

    percent stats & db merging

--- Team20ScoutingClient/MainWindow.xaml.cs
+++ Team20ScoutingClient/MainWindow.xaml.cs
@@ -1,23 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
 
 namespace Team20ScoutingClient {
 	public partial class MainWindow : Window {
 		#region Main
-		private DBClient db;
-
-		private bool init;
-
 		public MainWindow() {
 			InitializeComponent();
-
-			db = new DBClient("C:/Users/Andrew/source/repos/Team20ScoutingClient/2019ScoutingData.sqlite");
-
-			init = false;
 
 			InitTabs();
 			RefreshTabs();
@@ -25,14 +15,13 @@ namespace Team20ScoutingClient {
 
 		private void InitTabs() {
 			InitTeamStatsTab();
-			InitTeamTrendsTab();
+			//InitTeamTrendsTab();
 			InitDataTab();
-			init = true;
 		}
 
 		private void RefreshTabs() {
 			RefreshTeamStatsTab();
-			RefreshTeamTrendsTab();
+			//RefreshTeamTrendsTab();
 			RefreshDataTab();
 		}
 
@@ -66,108 +55,72 @@ namespace Team20ScoutingClient {
 			maxPointsStat;
 
 		private void InitTeamStatsTab() {
-			habLineL1Stat = new Stat(ref habLineL1TB, "L1", "%", () => {
-				if (db.GetData("RawData", new string[] { "StartPosition", "CrossHabLine" }, filter: "TeamNumber", filterValue: TeamStatsSelection.SelectedItem.ToString(), orderBy: "MatchNumber")) {
-					int freq = 0;
-					int count = 0;
-					int total = db.Data[0].Count;
-					for (int i = 0; i < total; i++)
-						if (db.Data[0][i] == "3" || db.Data[0][i] == "4" || db.Data[0][i] == "5") {
-							count++;
-							if (db.Data[1][i] == "1")
-								freq++;
-						}
-					if (count == 0)
-						return null;
-					return Math.Round((double)freq / count * 100, 2);
-				}
-				return null;
-			});
-			habLineL2Stat = new Stat(ref habLineL2TB, "L2", "%", () => {
-				if (db.GetData("RawData", new string[] { "StartPosition", "CrossHabLine" }, filter: "TeamNumber", filterValue: TeamStatsSelection.SelectedItem.ToString(), orderBy: "MatchNumber")) {
-					int freq = 0;
-					int count = 0;
-					int total = db.Data[0].Count;
-					for (int i = 0; i < total; i++)
-						if (db.Data[0][i] == "1" || db.Data[0][i] == "2") {
-							count++;
-							if (db.Data[1][i] == "1")
-								freq++;
-						}
-					if (count == 0)
-						return null;
-					return Math.Round((double)freq / count * 100, 2);
-				}
-				return null;
-			});
-			climbL1Stat = new Stat(ref climbL1TB, "L1", "%", () => {
-				if (db.GetData("RawData", new string[] { "HabLevelAchieved", "HabLevelAttempted" }, filter: "TeamNumber", filterValue: TeamStatsSelection.SelectedItem.ToString(), orderBy: "MatchNumber")) {
-					int achieved = 0;
-					int attempted = 0;
-					int total = db.Data[0].Count;
-					for (int i = 0; i < total; i++) {
-						if (db.Data[0][i] == "1")
-							achieved++;
-						if (db.Data[1][i] == "1")
-							attempted++;
-					}
-					if (attempted == 0)
-						return null;
-					return Math.Round((double)achieved / attempted * 100, 2);
-				}
-				return null;
-			});
-			climbL2Stat = new Stat(ref climbL2TB, "L1", "%", () => {
-				if (db.GetData("RawData", new string[] { "HabLevelAchieved", "HabLevelAttempted" }, filter: "TeamNumber", filterValue: TeamStatsSelection.SelectedItem.ToString(), orderBy: "MatchNumber")) {
-					int achieved = 0;
-					int attempted = 0;
-					int total = db.Data[0].Count;
-					for (int i = 0; i < total; i++) {
-						if (db.Data[0][i] == "2")
-							achieved++;
-						if (db.Data[1][i] == "2")
-							attempted++;
-					}
-					if (attempted == 0)
-						return null;
-					return Math.Round((double)achieved / attempted * 100, 2);
-				}
-				return null;
-			});
-			climbL3Stat = new Stat(ref climbL3TB, "L1", "%", () => {
-				if (db.GetData("RawData", new string[] { "HabLevelAchieved", "HabLevelAttempted" }, filter: "TeamNumber", filterValue: TeamStatsSelection.SelectedItem.ToString(), orderBy: "MatchNumber")) {
-					int achieved = 0;
-					int attempted = 0;
-					int total = db.Data[0].Count;
-					for (int i = 0; i < total; i++) {
-						if (db.Data[0][i] == "3")
-							achieved++;
-						if (db.Data[1][i] == "3")
-							attempted++;
-					}
-					if (attempted == 0)
-						return null;
-					return Math.Round((double)achieved / attempted * 100, 2);
-				}
-				return null;
-			});
+			habLineL1Stat = new Stat("%");
+			habLineL2Stat = new Stat("%");
+			climbL1Stat = new Stat("%");
+			climbL2Stat = new Stat("%");
+			climbL3Stat = new Stat("%");
 			stats = new Stat[] { habLineL1Stat, habLineL2Stat, climbL1Stat, climbL2Stat, climbL3Stat };
-			if (db.GetData("teams", new string[] { "number", "name" }, orderBy: "number")) {
-				TeamStatsSelection.Items.Clear();
-				foreach (string item in db.Data[0])
-					TeamStatsSelection.Items.Add(item);
-				TeamStatsSelection.SelectedItem = TeamStatsSelection.Items[0];
-			}
+			TeamStatsSelection.Items.Clear();
+			List<double> teams = DBClient.ExecuteQuery(
+				"SELECT TeamNumber " +
+				"FROM RawData " +
+				"GROUP BY TeamNumber " +
+				"ORDER BY TeamNumber ASC;",
+				true
+			);
+			foreach (double team in teams)
+				TeamStatsSelection.Items.Add(team);
+			TeamStatsSelection.SelectedItem = TeamStatsSelection.Items[0];
 		}
 
 		private void RefreshTeamStatsTab() {
+			// get teams from database
+			List<double> teams = DBClient.ExecuteQuery(
+				"SELECT TeamNumber " +
+				"FROM RawData " +
+				"GROUP BY TeamNumber " +
+				"ORDER BY TeamNumber ASC;",
+				true
+			);
+			// add teams to combobox that aren't already there
+			foreach (double team in teams)
+				if (!TeamStatsSelection.Items.Contains(team))
+					TeamStatsSelection.Items.Add(team);
+			// create SQL queries
+			habLineL1Stat.Query =
+				"SELECT 100.0 * SUM(CrossHabLine) / COUNT() " +
+				"FROM RawData " +
+				"WHERE TeamNumber = " + TeamStatsSelection.SelectedItem.ToString() + " AND StartPosition BETWEEN 3 AND 5;";
+			habLineL2Stat.Query =
+				"SELECT 100.0 * SUM(CrossHabLine) / COUNT() " +
+				"FROM RawData " +
+				"WHERE TeamNumber = " + TeamStatsSelection.SelectedItem.ToString() + " AND StartPosition BETWEEN 1 AND 2;";
+			climbL1Stat.Query =
+				"SELECT 100.0 * COUNT(CASE WHEN HabLevelAchieved = 1 THEN HabLevelAchieved ELSE NULL END) / COUNT() " +
+				"FROM RawData " +
+				"WHERE TeamNumber = " + TeamStatsSelection.SelectedItem.ToString() + " AND HabLevelAttempted = 1;";
+			climbL2Stat.Query =
+				"SELECT 100.0 * COUNT(CASE WHEN HabLevelAchieved = 2 THEN HabLevelAchieved ELSE NULL END) / COUNT() " +
+				"FROM RawData " +
+				"WHERE TeamNumber = " + TeamStatsSelection.SelectedItem.ToString() + " AND HabLevelAttempted = 2;";
+			climbL3Stat.Query =
+				"SELECT 100.0 * COUNT(CASE WHEN HabLevelAchieved = 3 THEN HabLevelAchieved ELSE NULL END) / COUNT() " +
+				"FROM RawData " +
+				"WHERE TeamNumber = " + TeamStatsSelection.SelectedItem.ToString() + " AND HabLevelAttempted = 3;";
+			// update values of all stats
 			foreach (Stat s in stats)
-				s.Calculate();
+				s.Update();
+			// display stat values in UI
+			habLineL1TB.Text = habLineL1Stat.ToString();
+			habLineL2TB.Text = habLineL2Stat.ToString();
+			climbL1TB.Text = climbL1Stat.ToString();
+			climbL2TB.Text = climbL2Stat.ToString();
+			climbL3TB.Text = climbL3Stat.ToString();
 		}
 
 		private void TeamStatsSelection_SelectionChanged(object sender, SelectionChangedEventArgs e) {
-			if (init)
-				RefreshTeamStatsTab();
+			RefreshTeamStatsTab();
 		}
 
 		private List<double> ConvertList(List<string> stringList) {
@@ -193,53 +146,52 @@ namespace Team20ScoutingClient {
 			teamTeleopCargo = new LineGraph(ref teamTeleopCargoCanvas, 0, 10, "Cargo in Teleop");
 			teamTeleopPanel = new LineGraph(ref teamTeleopPanelCanvas, 0, 10, "Hatch Panels in Teleop");
 			teamLineGraphs = new LineGraph[] { teamSandstormCargo, teamSandstormPanel, teamTeleopCargo, teamTeleopPanel };
-			if (db.GetData("teams", new string[] { "number", "name" }, orderBy: "number")) {
+			if (DBClient.GetData("teams", new string[] { "number", "name" }, orderBy: "number")) {
 				TeamTrendsSelection.Items.Clear();
-				foreach (string item in db.Data[0])
+				foreach (string item in DBClient.Data[0])
 					TeamTrendsSelection.Items.Add(item);
 				TeamTrendsSelection.SelectedItem = TeamTrendsSelection.Items[0];
 			}
 		}
 
 		private void RefreshTeamTrendsTab() {
-			if (db.GetData("teams", new string[] { "number", "name" }, filter: "number", filterValue: TeamTrendsSelection.SelectedItem.ToString(), orderBy: "number"))
-				TeamsTabTitle.Text = db.Data[0][0] + " - " + db.Data[1][0];
+			if (DBClient.GetData("teams", new string[] { "number", "name" }, filter: "number", filterValue: TeamTrendsSelection.SelectedItem.ToString(), orderBy: "number"))
+				TeamsTabTitle.Text = DBClient.Data[0][0] + " - " + DBClient.Data[1][0];
 			foreach (LineGraph lineGraph in teamLineGraphs)
 				lineGraph.LinePlots.Clear();
-			if (db.GetData("sandstorm", new string[] { "cargoCollect", "cargoDeliverShip", "cargoDeliverRocket", "cargoDrop" }, filter: "team", filterValue: TeamTrendsSelection.SelectedItem.ToString(), orderBy: "match")) {
-				teamSandstormCargo.LinePlots.Add(new LinePlot(db.Data[0], Brushes.Yellow, false));
-				teamSandstormCargo.LinePlots.Add(new LinePlot(db.Data[1], Brushes.Green, true));
-				teamSandstormCargo.LinePlots.Add(new LinePlot(db.Data[2], Brushes.Green, false));
-				teamSandstormCargo.LinePlots.Add(new LinePlot(db.Data[3], Brushes.Red, false));
-			}
-			if (db.GetData("sandstorm", new string[] { "panelCollectStation", "panelCollectFloor", "panelDeliverShip", "panelDeliverRocket", "panelDrop" }, filter: "team", filterValue: TeamTrendsSelection.SelectedItem.ToString(), orderBy: "match")) {
-				teamSandstormPanel.LinePlots.Add(new LinePlot(db.Data[0], Brushes.Yellow, false));
-				teamSandstormPanel.LinePlots.Add(new LinePlot(db.Data[1], Brushes.Yellow, true));
-				teamSandstormPanel.LinePlots.Add(new LinePlot(db.Data[2], Brushes.Green, true));
-				teamSandstormPanel.LinePlots.Add(new LinePlot(db.Data[3], Brushes.Green, false));
-				teamSandstormPanel.LinePlots.Add(new LinePlot(db.Data[4], Brushes.Red, false));
-			}
-			if (db.GetData("teleop", new string[] { "cargoCollectStation", "cargoCollectFloor", "cargoDeliverShip", "cargoDeliverRocket", "cargoDrop" }, filter: "team", filterValue: TeamTrendsSelection.SelectedItem.ToString(), orderBy: "match")) {
-				teamTeleopCargo.LinePlots.Add(new LinePlot(db.Data[0], Brushes.Yellow, false));
-				teamTeleopCargo.LinePlots.Add(new LinePlot(db.Data[1], Brushes.Yellow, true));
-				teamTeleopCargo.LinePlots.Add(new LinePlot(db.Data[2], Brushes.Green, true));
-				teamTeleopCargo.LinePlots.Add(new LinePlot(db.Data[3], Brushes.Green, false));
-				teamTeleopCargo.LinePlots.Add(new LinePlot(db.Data[4], Brushes.Red, false));
-			}
-			if (db.GetData("teleop", new string[] { "panelCollectStation", "panelCollectFloor", "panelDeliverShip", "panelDeliverRocket", "panelDrop" }, filter: "team", filterValue: TeamTrendsSelection.SelectedItem.ToString(), orderBy: "match")) {
-				teamTeleopPanel.LinePlots.Add(new LinePlot(db.Data[0], Brushes.Yellow, false));
-				teamTeleopPanel.LinePlots.Add(new LinePlot(db.Data[1], Brushes.Yellow, true));
-				teamTeleopPanel.LinePlots.Add(new LinePlot(db.Data[2], Brushes.Green, true));
-				teamTeleopPanel.LinePlots.Add(new LinePlot(db.Data[3], Brushes.Green, false));
-				teamTeleopPanel.LinePlots.Add(new LinePlot(db.Data[4], Brushes.Red, false));
-			}
-			foreach (LineGraph lineGraph in teamLineGraphs)
-				lineGraph.Draw();
+			//if (DBClient.GetData("sandstorm", new string[] { "cargoCollect", "cargoDeliverShip", "cargoDeliverRocket", "cargoDrop" }, filter: "team", filterValue: TeamTrendsSelection.SelectedItem.ToString(), orderBy: "match")) {
+			//	teamSandstormCargo.LinePlots.Add(new LinePlot(DBClient.Data[0], Brushes.Yellow, false));
+			//	teamSandstormCargo.LinePlots.Add(new LinePlot(DBClient.Data[1], Brushes.Green, true));
+			//	teamSandstormCargo.LinePlots.Add(new LinePlot(DBClient.Data[2], Brushes.Green, false));
+			//	teamSandstormCargo.LinePlots.Add(new LinePlot(DBClient.Data[3], Brushes.Red, false));
+			//}
+			//if (DBClient.GetData("sandstorm", new string[] { "panelCollectStation", "panelCollectFloor", "panelDeliverShip", "panelDeliverRocket", "panelDrop" }, filter: "team", filterValue: TeamTrendsSelection.SelectedItem.ToString(), orderBy: "match")) {
+			//	teamSandstormPanel.LinePlots.Add(new LinePlot(DBClient.Data[0], Brushes.Yellow, false));
+			//	teamSandstormPanel.LinePlots.Add(new LinePlot(DBClient.Data[1], Brushes.Yellow, true));
+			//	teamSandstormPanel.LinePlots.Add(new LinePlot(DBClient.Data[2], Brushes.Green, true));
+			//	teamSandstormPanel.LinePlots.Add(new LinePlot(DBClient.Data[3], Brushes.Green, false));
+			//	teamSandstormPanel.LinePlots.Add(new LinePlot(DBClient.Data[4], Brushes.Red, false));
+			//}
+			//if (DBClient.GetData("teleop", new string[] { "cargoCollectStation", "cargoCollectFloor", "cargoDeliverShip", "cargoDeliverRocket", "cargoDrop" }, filter: "team", filterValue: TeamTrendsSelection.SelectedItem.ToString(), orderBy: "match")) {
+			//	teamTeleopCargo.LinePlots.Add(new LinePlot(DBClient.Data[0], Brushes.Yellow, false));
+			//	teamTeleopCargo.LinePlots.Add(new LinePlot(DBClient.Data[1], Brushes.Yellow, true));
+			//	teamTeleopCargo.LinePlots.Add(new LinePlot(DBClient.Data[2], Brushes.Green, true));
+			//	teamTeleopCargo.LinePlots.Add(new LinePlot(DBClient.Data[3], Brushes.Green, false));
+			//	teamTeleopCargo.LinePlots.Add(new LinePlot(DBClient.Data[4], Brushes.Red, false));
+			//}
+			//if (DBClient.GetData("teleop", new string[] { "panelCollectStation", "panelCollectFloor", "panelDeliverShip", "panelDeliverRocket", "panelDrop" }, filter: "team", filterValue: TeamTrendsSelection.SelectedItem.ToString(), orderBy: "match")) {
+			//	teamTeleopPanel.LinePlots.Add(new LinePlot(DBClient.Data[0], Brushes.Yellow, false));
+			//	teamTeleopPanel.LinePlots.Add(new LinePlot(DBClient.Data[1], Brushes.Yellow, true));
+			//	teamTeleopPanel.LinePlots.Add(new LinePlot(DBClient.Data[2], Brushes.Green, true));
+			//	teamTeleopPanel.LinePlots.Add(new LinePlot(DBClient.Data[3], Brushes.Green, false));
+			//	teamTeleopPanel.LinePlots.Add(new LinePlot(DBClient.Data[4], Brushes.Red, false));
+			//}
+			//foreach (LineGraph lineGraph in teamLineGraphs)
+			//	lineGraph.Draw();
 		}
 
 		private void TeamTrendsSelection_SelectionChanged(object sender, SelectionChangedEventArgs e) {
-			if (init)
-				RefreshTeamTrendsTab();
+			RefreshTeamTrendsTab();
 		}
 		#endregion
 
@@ -291,6 +243,10 @@ namespace Team20ScoutingClient {
 					for (int i = tokenSources.Count - 1; i >= 0; i--)
 						tokenSources.Pop().Cancel();
 			}
+		}
+
+		private void MergeButton_Click(object sender, RoutedEventArgs e) {
+			DBClient.Merge("C:/Users/Andrew/Desktop/", "2019_test");
 		}
 		#endregion
 	}
